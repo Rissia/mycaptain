7abdf47baa70c340ddaac14825f3ae24352b6e05
Rissia <65309619+Rissia@users.noreply.github.com>
2020-05-14 01:05:00 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,3 +1,6 @@
 r=eval(input("Input the radius of the circle:\n"))
-area=22*(r**2)/7
-print("The area of the circle with radius",r,"is",area)
+if r>0:
+  area=22*(r**2)/7
+  print("The area of the circle with radius",r,"is",area)
+else:
+  print("Radius value entered must be positive")
